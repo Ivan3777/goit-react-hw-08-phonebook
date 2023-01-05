0be8f9571e708602fe03012f03d6ad035851fa97
Ivan Vodopianov <vis3010@gmail.com>
2023-01-05 16:56:36 +0200

    end styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -51,5 +51,5 @@ export const App = () => {
         />
       </Route>
     </Routes>
- );
+  );
 };
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,17 @@
-import { Dna } from 'react-loader-spinner';
+import { Blocks } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
-export const Loader = () => (
-  <div className={css.loader}>
-    <Dna
-      visible={true}
-      height="80"
-      width="80"
-      ariaLabel="dna-loading"
-      wrapperStyle={{}}
-      wrapperClass="dna-wrapper"
-    />
-  </div>
-);
+export const Loader = () => {
+  return (
+    <div className={css.loader}>
+      <Blocks
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+      />
+    </div>
+  );
+};
